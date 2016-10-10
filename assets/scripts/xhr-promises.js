831277a02494ad08ad890184d52457dc4957842b
rebekahheacock <rebekah.heacock@gmail.com>
2016-10-10 16:06:00 -0400

    Add comments

--- assets/scripts/xhr-promises.js
+++ assets/scripts/xhr-promises.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const getFormFields = require('../../lib/get-form-fields');
+
 $(() => {
   // // console methods require `this` to be `console`
   // // promise function are called with `this === undefined`
   // let clog = console.log.bind(console);
   // let elog = console.error.bind(console);
 
-  const baseUrl = 'http://localhost:3000/';
+  const baseUrl = 'http://localhost:3000';
 
   const onError = (error) => {
     console.error(error);
@@ -22,11 +24,24 @@ $(() => {
     console.log('Signed up');
   };
 
+  // signUpOrIn returns a promise
+  // signIn returns signUpOrIn(), which returns a promise
+
   const signUpOrIn = (credentials, path) =>
     new Promise((resolve, reject) => {
+      // promise is implicitly returned because you're using
+      // a fat arrow function with no { }
+      // resolve and reject replace success and failure handlers
+      // no longer define explicitly
+      // let promise maintain the callback chain instead
+      // when using promises: resolve takes replace of initial success callback
+      // reject takes place of initial failure callback
       let xhr = new XMLHttpRequest();
       xhr.addEventListener('load', () => {
         if (xhr.status >= 200 && xhr.status < 300) {
+          // can't resolve promise until load event fires
+          // that's what makes it asynchronous
+          // load gives us the necessary data to resolve the promise
           resolve(xhr.response);
         } else {
           reject(xhr);
@@ -34,24 +49,39 @@ $(() => {
       });
       xhr.addEventListener('error', () => reject(xhr));
       xhr.open('POST', baseUrl + path);
-      xhr.send(credentials);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify(credentials));
     });
 
+  // don't have to pass onFulfilled or onRejected
+  // promises make things cleaner!
   const signIn = (credentials) => signUpOrIn(credentials, '/sign-in');
 
   const signUp = (credentials) => signUpOrIn(credentials, '/sign-up');
 
-  $('#sign-up-promise').on('submit', function submitHandler(e) {
+  const submitHandler = (event) => {
     // // console methods require `this` to be `console`
-    // // promise function are called with `this === undefined`
+    // // promise functions are called with `this === undefined`
     // let clog = console.log.bind(console);
     // let elog = console.error.bind(console);
-    e.preventDefault();
-    let formData = new FormData(this);
-    signUp(formData)
+    // not necessarily saving lines of code here, but:
+      // shortening all function signatures
+      // improving clarity: everything reads in order it executes even though it's async
+      // allowed to do this b/c promises give us future values
+      // future values allow us to queue functions that will execute when those values resolve
+    event.preventDefault();
+    let data = getFormFields(event.target);
+    signUp(data)
     .then(onSignUp)
-    .then(() => signIn(formData))
+    .then(() => signIn(data)) // passing original data, not response, here. also passing a function, not a function invocation
     .then(onSignIn)
     .catch(onError);
-  });
+  };
+
+  // () => signIn(data) is the same as
+  // function () {
+  //   return signIn(data);
+  // }
+
+  $('#sign-up-promise').on('submit', submitHandler);
 });
--- assets/scripts/xhr.js
+++ assets/scripts/xhr.js
@@ -1,12 +1,21 @@
 'use strict';
 
+// TO SUM UP
+// this is callback hell
+// keep bouncing back and forth; chain is unclear
+
+const getFormFields = require('../../lib/get-form-fields');
+
+// jQuery shorthand for $(document).ready
 $(() => {
   const baseUrl = 'http://localhost:3000';
 
+  // error handler
   const onError = (error) => {
     console.error(error);
   };
 
+  // more handlers
   const onSignUp = (response) => {
     console.log(response);
     console.log('Signed up');
@@ -17,34 +26,83 @@ $(() => {
     console.log('Signed in');
   };
 
-  const signUpOrIn = (credentials, path, onFulilled, onRejected) => {
+  // will sign up OR sign in
+  // two requests only differ by the URL path
+  // onFulfilled: success handler
+  // onRejected: failure handler
+  const signUpOrIn = (credentials, path, onFulfilled, onRejected) => {
+    // XMLHttpRequest is a constructor function: capital letter, "new", invoked()
+    // creating request object does not initiate request
+    // request object doesn't (by default) have what it needs to make request
     let xhr = new XMLHttpRequest();
+
+    // 'load' is listening for response from server
     xhr.addEventListener('load', () => {
+      // status range 200-299: success
       if (xhr.status >= 200 && xhr.status < 300) {
-        onFulilled(xhr.response);
+        // response is whatever comes back from server
+        // can parse it here (if it's JSON) or parse it in your handler
+        // can do whatever you want before passing it on to onFulfilled
+        onFulfilled(xhr.response);
       } else {
+        // fire failure handler
+        // pass it the entire request object (xhr)
+        // pass the entire object so we can handle things different in different cases
+        // e.g., bad request vs good request that wasn't authorized vs wrong URL
         onRejected(xhr);
       }
     });
+
+    // if there's an error for any non-status-code reason
+    // errors that occur before request is made
+      // note that bad data will mean we get back a "bad" status code
+      // other bad things can happen too, though:
+      // ex: forget to set client origin
+        // browser generates CORS-related errors
+        // before it fires AJAX request, it fires an options request
+        // open up network inspector to see this
+        // server responds with what actions are allowed on domain
+        // errors here aren't about response status for request
+    // this is clunky: we're handling errors in multiple places.
     xhr.addEventListener('error', () => onRejected(xhr));
+
+    // start request
     xhr.open('POST', baseUrl + path);
-    xhr.send(credentials);
+    // have to set request header to accept json
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    // make request/send data
+    // have to send as JSON, not object
+    xhr.send(JSON.stringify(credentials));
   };
 
-  const signIn = (credentials, onFulilled, onRejected) =>
-    signUpOrIn(credentials, '/sign-in', onFulilled, onRejected);
+  // uses path for sign in
+  // "method delegation": applying static data to method
+  // might be related to "partial application," but that's something for functional programming experts
+  // in Ruby: "method dispatch"
+  const signIn = (credentials, onFulfilled, onRejected) =>
+    signUpOrIn(credentials, '/sign-in', onFulfilled, onRejected);
 
-  const signUp = (credentials, onFulilled, onRejected) =>
-    signUpOrIn(credentials, '/sign-up', onFulilled, onRejected);
+  // uses path for sign up
+  const signUp = (credentials, onFulfilled, onRejected) =>
+    signUpOrIn(credentials, '/sign-up', onFulfilled, onRejected);
 
-  $('#sign-up').on('submit', function submitHandler(e) {
-    e.preventDefault();
-    let formData = new FormData(this);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    let data = getFormFields(event.target);
+
+    // auto sign in!
     const onSignUpSuccess = function (response) {
+      // original success handler defined above
       onSignUp(response);
-      signIn(formData, onSignIn, onError);
+      // let's do other stuff, too!
+      // "callback chain"
+      signIn(data, onSignIn, onError);
     };
 
-    signUp(formData, onSignUpSuccess, onError);
-  });
+    // this is the important part of the submitHandler function
+    signUp(data, onSignUpSuccess, onError);
+  };
+
+  // attach handler to sign up form
+  $('#sign-up').on('submit', submitHandler);
 });
